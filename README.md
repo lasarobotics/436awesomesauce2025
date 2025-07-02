2ab8825210dff41b4cb2a9f63ed6a75d033c9ad5
saturnvi <maxwang420@gmail.com>
2025-07-02 17:04:41 -0500

    create drive stuff, figure out later

---
+++ README.md
@@ -0,0 +1,3 @@
+# 436 (Purplexity) Robot Code
+
+created using finite state machines (courtesy of purplelib) and yagsl
---
+++ src/main/java/frc/robot/subsystems/drive/SwerveManager.java
@@ -0,0 +1,21 @@
+package frc.robot.subsystems.drive;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.util.Units;
+
+public class SwerveManager {
+ 
+    SwerveDriveKinematics kinematics;
+
+    public SwerveManager() {
+
+        // phlapjack actually has the same dimensions as the example they give!
+        kinematics = new SwerveDriveKinematics(
+            new Translation2d(Units.inchesToMeters(12.5), Units.inchesToMeters(12.5)), // Front Left
+            new Translation2d(Units.inchesToMeters(12.5), Units.inchesToMeters(-12.5)), // Front Right
+            new Translation2d(Units.inchesToMeters(-12.5), Units.inchesToMeters(12.5)), // Back Left
+            new Translation2d(Units.inchesToMeters(-12.5), Units.inchesToMeters(-12.5))  // Back Right
+        );
+    }
+}
